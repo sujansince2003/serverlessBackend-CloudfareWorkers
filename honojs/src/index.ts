e267d4009a164439eeb0751d39c69fd0edad0af1
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-02-21 00:52:29 +0545

    a basic implmentation on honojs

--- honojs/src/index.ts
+++ honojs/src/index.ts
@@ -3,7 +3,22 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.json({
+    message: 'Hello World',
+  })
 })
+
+
+
+app.post("/user", async (c) => {
+  const body = await c.req.json()
+  const token = c.req.header("Authorization")
+  const query = c.req.query("name")
+
+  return c.json({ msg: "got it", body, token, query })
+
+}
+
+)
 
 export default app
